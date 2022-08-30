770b7ca05bff7793aad4b3a2f061f42ad2966d82
Matheus Baron <matheusbaron10@gmail.com>
2022-08-30 15:05:29 -0300

    Feat: default theme init

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,24 @@
+const defaultTheme = {
+  colors: {
+    red: {
+      main: "#DC1637"
+    },
+    white: {
+      main: "#FFFFFF"
+    },
+    back: {
+      main: "#000000"
+    },
+    gray: {
+      100: "#EBEBF0",
+      200: "#E5E5E5",
+      300: "#DEDEE3",
+      400: "#AEAEB3",
+      500: "#7A7A80",
+      600: "#47474D",
+      700: "#1B1B1F"
+    },
+  }
+}
+
+export { defaultTheme }
