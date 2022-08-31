82082811c4b45fd16ed36a9b63da66e3bc7265ce
Matheus Baron <matheusbaron10@gmail.com>
2022-08-31 18:43:13 -0300

    Feat: api created

--- src/pages/Cars/List/index.tsx
+++ src/pages/Cars/List/index.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppBar } from '../../../components/AppBar';
 import { Layout } from '../../../components/Layout';
+import { api } from '../../../services/api';
 import * as S from './styles';
 
 const CarsList: React.FC = () => {
+  const [cars, setCars] = useState([])
+
+  useEffect(() => {
+    api.get('/cars/available')
+      .then(response => setCars(response.data))
+  }, [])
+
   return (
     <Layout
       appBarComponent={<AppBar title='Início' />}
