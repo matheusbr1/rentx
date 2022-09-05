e186ef919530920347ecf3b7721427fb56461ede
Matheus Baron <matheusbaron10@gmail.com>
2022-09-05 16:31:42 -0300

    Feat: SpecCard component created

---
+++ src/components/SpecCard/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import * as S from './styles';
+
+interface SpecCardProps {
+  text: string
+  image: string
+}
+
+const SpecCard: React.FC<SpecCardProps> = ({ text, image }) => {
+  return (
+    <S.SpecCardContainer key={text} >
+      <S.SpecCardImage>
+        <img src={image} alt={text} />
+      </S.SpecCardImage>
+
+      <S.SpecCardText>
+        <p>{text}</p>
+      </S.SpecCardText>
+    </S.SpecCardContainer>
+  )
+}
+
+export { SpecCard }
---
+++ src/components/SpecCard/styles.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { pxToRem } from "../../styles/utils/pxToRem";
+
+export const SpecCardContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: ${pxToRem(2)};
+
+  border-bottom: solid 2px #EBEBF0;
+`
+
+export const SpecCardImage = styled.div`
+  display: flex;
+  align-items: center;
+
+  height: 100%;
+
+  background: #F4F5F6;
+  padding: ${pxToRem(16)};
+`
+
+export const SpecCardText = styled.div`
+  display: flex;
+  align-items: center;
+
+  height: 100%;
+
+  background: #F4F5F6;
+  padding: ${pxToRem(16)};
+  
+  p {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 22px;
+    color: #7A7A80;
+  }
+`
